72f4c3c34c0c7d4c709c76a384a47ffcb2a33d54
santiago000777 <jjjjjxxxxx892@gmail.com>
2024-11-08 14:09:59 +0100

    Pridani Insert a Bubble

--- Cviceni8/main.c
+++ Cviceni8/main.c
@@ -86,11 +86,31 @@ void InsertSort_zarazka_zacatek(int* pole, int pocet) {
 }
 
 void InsertSort_zarazka_konec(int* pole, int pocet) {
-
+    pocet++;
+    int i;
+    for (int d = pocet - 3; d >= 0; d--) {
+        pole[pocet - 1] = pole[d];
+        i = d;
+        while (pole[pocet - 1] > pole[i + 1]) {
+            pole[i] = pole[i + 1];
+            i++;
+        }
+        pole[i] = pole[pocet - 1];
+    }
 }
 
 void BubbleSort(int* pole, int pocet) {
-
+    int pom;
+    for (int d = 0; d < pocet - 2; d++) {
+        vypis(pole, pocet);
+        for (int i = pocet - 1; i >= d + 1; i--) {
+            if (pole[i - 1] > pole[i]) {
+                pom = pole[i];
+                pole[i] = pole[i - 1];
+                pole[i - 1] = pom;
+            }
+        }
+    }
 }
 
 void RippleSort(int* pole, int pocet) {
