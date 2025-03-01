33d9849dbc4d1fb090991f1d2ea5e11b82bef3da
Jakub Veit <jakub@veit.cz>
2025-03-01 18:07:09 +0100

    Funkcni razeni podle veku cv14

--- Cviceni14/main.c
+++ Cviceni14/main.c
@@ -12,42 +12,79 @@
 */
 
 typedef struct {
-    char name[20];
+    char name[21];
     int age;
-    char country[30];
+    char country[31];
 
 } Entity;
 
-void Print 
+int LoadArray(FILE* file, Entity* array, int count) {
+    Entity temp;
+    int index = 0;
+    while (index < count && fscanf(file, "%20s%d%30s", temp.name, &temp.age, temp.country) == 3) {
+        array[index] = temp;
+        index++;
+    }
 
-void InsertSort(int* pole, int* poleIndex, int pocet) { // predelat na 
-    int vkladany, i;
-    for (int d = 1; d < pocet; d++) {
-        vkladany = pole[d];
-        i = d;
-        while (i > 0 && pole[i - 1] > vkladany) {
-            pole[i] = pole[i - 1];
-            i--;
-        }
-        pole[i] = vkladany;
+    return index;
+}
+
+void PrintArray(FILE* file, const Entity* array, const int* indexArray, int count) {
+    for (int i = 0; i < count; i++) {
+        fprintf(file, "%d\t%s\t%d\t%s\n", indexArray[i], array[indexArray[i]].name, array[indexArray[i]].age, array[indexArray[i]].country);
     }
 }
 
-void IndexInsertSort(Entity* pole, int* poleIndex, int pocet) { // predelat na 
+void InsertSort(int* array, int* indexArray, int count) { // predelat na 
     int vkladany, i;
-    for (int d = 1; d < pocet; d++) {
-        vkladany = pole[d];
+    for (int d = 1; d < count; d++) {
+        vkladany = array[d];
         i = d;
-        while (i > 0 && pole[i - 1] > vkladany) {
-            pole[i] = pole[i - 1];
+        while (i > 0 && array[i - 1] > vkladany) {
+            array[i] = array[i - 1];
             i--;
         }
-        pole[i] = vkladany;
+        array[i] = vkladany;
+    }
+}
+
+void AgeIndexInsert(Entity* array, int* indexArray, int count) { // predelat na 
+    int vkladany, i, vkladanyIndex;
+    for (int d = 1; d < count; d++) {
+        vkladanyIndex = indexArray[d];
+        vkladany = array[vkladanyIndex].age;
+        i = d;
+
+        while (i > 0 && array[indexArray[i - 1]].age > vkladany) {
+            indexArray[i] = indexArray[i - 1];
+            i--;
+        }
+        indexArray[i] = vkladanyIndex;
     }
 }
 
 int main() {
-	
+    FILE* in = fopen("entities.txt", "r");
+    FILE* out = fopen("result.txt", "w");
+
+    if (in == NULL || out == NULL) {
+        return -1;
+    }
+
+    Entity entities[20];
+    int const count = LoadArray(in, entities, 20);
+
+    int* indexArray = malloc(4 * count);
+    for (int i = 0; i < count; i++) {
+        indexArray[i] = i;
+    }
+
+    AgeIndexInsert(entities, indexArray, count);
+
+    PrintArray(out, entities, indexArray, count);
+
+    fclose(in);
+    fclose(out);
 
 	return 0;
 }
